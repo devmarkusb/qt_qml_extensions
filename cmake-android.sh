c00c71d4e59f4858bacbb13b9b0d2b35180969db
MarkusB <>
2025-10-16 21:42:54 +0200

    qt up

--- cmake-android.sh
+++ cmake-android.sh
@@ -14,11 +14,7 @@ build_config=$1
 sign_key_path=$2
 sign_key_pass=$3
 subdir_lowercase=${1,,}
-build_dir=_build-$subdir_lowercase-android
-
-# set
-#-DUL_DEPLOYMENT_BUILD=ON \
-# below for deployment build
+build_dir=cmake-build-android-$subdir_lowercase
 
 mkdir -p $build_dir
 
@@ -26,24 +22,35 @@ cd $build_dir || exit
 
 sdk_path=$dev_sdk_path/Android/Sdk
 ndk_path=$sdk_path/ndk/28.0.13004108
-
-
-#    -DQT_CHAINLOAD_TOOLCHAIN_FILE=$ndk_path/build/cmake/android.toolchain.cmake \
-#    -DQT_ANDROID_SDK_BUILD_TOOLS_REVISION=36 \
+qt_version=6.9.3
 
 export QT_ANDROID_KEYSTORE_PATH=$sign_key_path
 export QT_ANDROID_KEYSTORE_ALIAS=key
 export QT_ANDROID_KEYSTORE_STORE_PASS=Q6KHNhs8tAFsPiYgbpoxo9FXfcQENzvu
 export QT_ANDROID_KEYSTORE_KEY_PASS=$sign_key_pass
 
-$dev_sdk_path/qt_linux/6.8.2/android_arm64_v8a/bin/qt-cmake \
+#    -DQT_CHAINLOAD_TOOLCHAIN_FILE=$ndk_path/build/cmake/android.toolchain.cmake \
+#    -DQT_ANDROID_SDK_BUILD_TOOLS_REVISION=36 \
+#    -DQT_ANDROID_ABIS="arm64-v8a,armeabi-v7a,x86,x86_64" \
+#    -DQT_ANDROID_BUILD_ALL_ABIS=TRUE \
+#    -DQT_ANDROID_MULTI_ABI_FORWARD_VARS=CMAKE_BUILD_TYPE \
+
+# set
+#-DUL_DEPLOYMENT_BUILD=ON \
+# below for deployment build
+# subdirs: android_arm64_v8a,android_armv7,android_x86,android_x86_64
+$dev_sdk_path/qt_linux/$qt_version/android_arm64_v8a/bin/qt-cmake \
     -DCMAKE_BUILD_TYPE=$build_config \
-    -DQT_ANDROID_BUILD_ALL_ABIS=TRUE \
+    -DQT_ANDROID_TARGET_SDK_VERSION=35 \
+    -DQT_ANDROID_MIN_SDK_VERSION=21 \
     -DANDROID_SDK_ROOT=$sdk_path \
     -DANDROID_NDK_ROOT=$ndk_path \
-    -DQT_ANDROID_TARGET_SDK_VERSION=35 \
-    -DCMAKE_PREFIX_PATH=$dev_sdk_path/qt_linux/6.8.2/gcc_64 \
+    -DQT_HOST_PATH=$dev_sdk_path/qt_linux/$qt_version/gcc_64 \
     -DQT_ANDROID_SIGN_AAB=True \
+    -DQT_ANDROID_PACKAGE_NAME=org.cismypa.qt_qml_extensions_testapp \
+    -DQT_ANDROID_VERSION_CODE=1 \
+    -DQT_ANDROID_VERSION_NAME=1 \
+    -DUL_DEPLOYMENT_BUILD=ON \
     -S .. -B . \
     -G Ninja
 
